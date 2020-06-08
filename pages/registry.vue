84b66a9a4e713abcdda751944ae5b8df66838a72
Noah Grubb <noah@Noahs-MacBook-Pro.local>
2020-06-08 16:06:29 -0400

    add registry links

--- pages/registry.vue
+++ pages/registry.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="page registry">
     <h1>Registry</h1>
-    <p class="coming-soon">Stay tuned! Details soon.</p>
+    <div class="page-content">
+      <p>We are registered at the following stores:</p>
+      <ul>
+        <li>
+          <a href="https://bit.ly/3dDrYwe" target="_blank" title="Crate &amp; Barrel Registry">Crate &amp; Barrel</a>
+        </li>
+        <li>
+          <a href="https://bit.ly/372CJ8G" target="_blank" title="West Elm Registry">West Elm</a>
+        </li>
+        <li>
+          <a href="https://bit.ly/3eSmyxn" target="_blank" title="Target Registry">Target</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -14,3 +27,35 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.registry {
+  font-size: 1.25em;
+
+  .page-content {
+    background-color: rgba(249, 232, 218, 0.7);
+    padding: 1rem;
+    margin: 0 1rem;
+    border-radius: .25rem;
+  }
+  
+
+  ul {
+    color: #E6B0A2;
+    font-weight: bold;
+    padding-left: 1.5rem;
+
+    li {
+      margin-top: 1rem;
+    }
+
+    a {
+      color: black;
+
+      &:hover {
+        color: #E6B0A2;
+      }
+    }
+  }
+}
+</style>
