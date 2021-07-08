72be1127df9e2a10598cd43ea7cb530ca5db7472
Noah Grubb <noah@blackfynn.com>
2021-07-08 15:18:33 -0400

    playing around with RSVP form

---
+++ components/RSVPForm/RSVPForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form
+    name="rsvp"
+    method="POST"
+    data-netlify="true"
+    data-netlify-honeypot="8jkxfg"
+  >
+    <input type="hidden" name="form-name" value="rsvp" />
+    <input type="hidden" name="8jkxfg" value="" />
+
+    <div class="field">
+      <label for="guest-guest">Name:</label>
+      <input type="text" name="guest-name" id="guest-name" value="" placeholder="The _____ family" />
+    </div>
+
+    <div class="field">
+      <label for="num-of-guests">Number in Party:</label>
+      <input type="number" name="num-of-guests" id="num-of-guests" increment="1" min="1" max="8" value="1" />
+    </div>
+
+    <button type="submit">Submit</button>
+  </form>
+
+</template>
+<script>
+export default {
+  name: "QAForm",
+  methods: {
+    updatePanelist (ev) {
+      this.currentPanelist = ev.target.value
+    }
+  },
+  data () {
+    return {
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/rsvp.vue
+++ pages/rsvp.vue
@@ -2,7 +2,21 @@
   <div class="page rsvp">
     <h1>RSVP</h1>
     <div class="page-content">
-      <p class="coming-soon">Coming soon.</p>
+
+      <form name="contact" method="POST" data-netlify="true">
+        <div class="field">
+          <label for="guest-guest">Name:</label>
+          <input type="text" name="guest-name" id="guest-name" value="" placeholder="The _____ family" />
+        </div>
+
+        <div class="field">
+          <label for="num-of-guests">Number in Party:</label>
+          <input type="number" name="num-of-guests" id="num-of-guests" increment="1" min="1" max="8" value="1" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
     </div>
   </div>
 </template>
